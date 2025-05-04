1d7a17b53502a4f8f08f897a959f43868ffc6e91
Ituking <77528963+Ituking@users.noreply.github.com>
2025-05-04 13:18:40 +0900

    chore: Flutterプロジェクト初期構成をfvm flutter createでセットアップ

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'auth/views/auth_view.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
@@ -26,4 +21,4 @@ class MyApp extends StatelessWidget {
       home: const AuthView(),
     );
   }
-} 
+}
