24491eff37582622cb078865a356ff9c98372de0
Ituking <77528963+Ituking@users.noreply.github.com>
2025-06-13 17:42:11 +0900

    FileDataモデルのFreezed化とJSON対応を追加
    - FileDataクラスをfreezedを用いた不変データモデルにリファクタリング
    - JSONシリアライズ/デシリアライズ機能を追加
    - 可読性と拡張性を向上

--- date_feed_back/lib/files/views/widgets/file_data.dart
+++ date_feed_back/lib/files/views/widgets/file_data.dart
@@ -1,24 +1,29 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'file_data.freezed.dart';
+part 'file_data.g.dart';
+
 /// ファイル情報を表すデータクラス。
 ///
 /// ファイル名、作成日、作成時刻、ユーザー名を保持する。
-class FileData {
-  /// ファイル名。
-  final String title;
-
-  /// 作成日（例: 2025/05/15）。
-  final String date;
-
-  /// 作成時刻（例: 12:50）。
-  final String time;
-
-  /// ユーザー名。
-  final String user;
-
+@freezed
+class FileData with _$FileData {
   /// [FileData] のインスタンスを生成する。
-  const FileData({
-    required this.title,
-    required this.date,
-    required this.time,
-    required this.user,
-  });
+  const factory FileData({
+    /// ファイル名。
+    required String title,
+
+    /// 作成日（例: 2025/05/15）。
+    required String date,
+
+    /// 作成時刻（例: 12:50）。
+    required String time,
+
+    /// ユーザー名。
+    required String user,
+  }) = _FileData;
+
+  /// JSON から [FileData] インスタンスを生成する。
+  factory FileData.fromJson(Map<String, dynamic> json) =>
+      _$FileDataFromJson(json);
 }
---
+++ date_feed_back/lib/files/views/widgets/file_data.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'file_data.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$FileDataImpl _$$FileDataImplFromJson(Map<String, dynamic> json) =>
+    _$FileDataImpl(
+      title: json['title'] as String,
+      date: json['date'] as String,
+      time: json['time'] as String,
+      user: json['user'] as String,
+    );
+
+Map<String, dynamic> _$$FileDataImplToJson(_$FileDataImpl instance) =>
+    <String, dynamic>{
+      'title': instance.title,
+      'date': instance.date,
+      'time': instance.time,
+      'user': instance.user,
+    };
