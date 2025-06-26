b5cefb3c74c82375d4a003c4e9c2e717f59d2d87
Ituking <77528963+Ituking@users.noreply.github.com>
2025-06-26 23:38:56 +0900

    feat: DateTime拡張を追加し、yyyy/MM/dd形式のフォーマット機能を実装
    - DateTimeクラスにtoJapaneseDate()メソッドを追加し、'yyyy/MM/dd'形式の文字列への変換をサポート
    - intlパッケージを使用して、ロケールに依存しない日付フォーマットを実現
    - formatters.dartに

--- lib/core/utils/formatters.dart
+++ lib/core/utils/formatters.dart
@@ -1,3 +1,14 @@
+import 'package:intl/intl.dart';
+
+/// [DateTime] を拡張し、日付関連のフォーマット機能を提供する。
+extension DateTimeFormat on DateTime {
+  /// [DateTime] を `yyyy/MM/dd` 形式の文字列に変換する。
+  String toJapaneseDate() {
+    final formatter = DateFormat('yyyy/MM/dd');
+    return formatter.format(this);
+  }
+}
+
 /// `int` 型を拡張し、時間関連のフォーマット機能を提供する拡張。
 extension DurationFormatter on int {
   /// 秒数を `MM:SS` 形式の文字列に変換する。
